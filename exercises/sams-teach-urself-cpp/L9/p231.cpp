32703a77707e3795fa4a2a92ebc160aa39cf9f0b
Your Name <you@example.com>
2022-12-09 08:25:59 -0800

    p231.cpp: added

---
+++ exercises/sams-teach-urself-cpp/L9/p231.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human {
+private:
+    string name;
+    int age;
+
+public:
+    Human(string humansName, int humansAge = 70) {
+        name = humansName;
+        age = humansAge;
+        cout << "overloaded constructor created name and age: " << name << age << endl;
+    }
+
+    void IntroduceSelf() {
+        cout << "I am " << name << " and age " << age << endl;
+    }
+};
+
+int main() {
+    Human firstMan("Adam", 25);
+    Human firstWoman("Eve", 28);
+    Human binary("Binary");
+    firstMan.IntroduceSelf();
+    firstWoman.IntroduceSelf();
+}

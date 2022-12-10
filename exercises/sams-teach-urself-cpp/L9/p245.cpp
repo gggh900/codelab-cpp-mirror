d51cd4b4c7e0f42b7dc1d0a40dca4ff38acd210c
Your Name <you@example.com>
2022-12-09 23:13:34 -0800

    p245.cpp: added

---
+++ exercises/sams-teach-urself-cpp/L9/p245.cpp
@@ -0,0 +1,64 @@
+// problem fixed in p238.cpp by using copy constructor.
+
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+class MyString {
+private:
+    char * buffer;
+
+public:
+    MyString(const char * pInitString) {
+        string initString = pInitString;
+        if (!initString.empty()) {
+            buffer = new char [initString.length() + 1];
+            strcpy(buffer, initString.c_str());
+            cout << "buffer points to: 0x" << hex << (unsigned int*)buffer << endl;
+        } else {
+            buffer = NULL;
+        }
+    }
+
+    // copy constructor
+    MyString(const MyString & copySource) {
+        buffer = NULL;
+        cout << "Copy constructor: copying from Mystring" << endl;
+        if (copySource.buffer != NULL) {
+        
+            // allocate own buffer.
+            
+            buffer = new char[strlen(copySource.buffer) + 1];
+
+            // deep copy from the source into local buffer.
+
+            strcpy(buffer, copySource.buffer);
+        
+            cout << "buffer points to: 0x" << hex << (unsigned int*)buffer << endl;
+        }
+    }
+
+    ~MyString() {
+        cout << "Invoking destructor, clearing up" << endl;
+        delete [] buffer;
+    }
+
+    int GetLength() {
+        return strlen(buffer);
+    }
+    
+    const char * GetString() {
+        return buffer;
+    }
+};
+
+MyString Copy(MyString& source) {
+    MyString copyForReturn(source.GetString());     // create copy.
+    return copyForReturn;
+}
+
+int main() {
+    MyString sayHello("Hello world of C++");
+    MyString sayHelloAgain(Copy(sayHello));
+}

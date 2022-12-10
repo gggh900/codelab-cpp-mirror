ac1d56fc6f76699ff2a861bf5f9ec47315468b83
Your Name <you@example.com>
2022-12-09 23:23:16 -0800

    p245.cpp: enhancement.

--- exercises/sams-teach-urself-cpp/L9/p245.cpp
+++ exercises/sams-teach-urself-cpp/L9/p245.cpp
@@ -1,4 +1,5 @@
 // problem fixed in p238.cpp by using copy constructor.
+// GG!! study further. Did not seem to work according to book...
 
 #include <iostream>
 #include <string>
@@ -15,7 +16,7 @@ public:
         if (!initString.empty()) {
             buffer = new char [initString.length() + 1];
             strcpy(buffer, initString.c_str());
-            cout << "buffer points to: 0x" << hex << (unsigned int*)buffer << endl;
+            cout << "Constructor: buffer points to: 0x" << hex << (unsigned int*)buffer << endl;
         } else {
             buffer = NULL;
         }
@@ -35,7 +36,17 @@ public:
 
             strcpy(buffer, copySource.buffer);
         
-            cout << "buffer points to: 0x" << hex << (unsigned int*)buffer << endl;
+            cout << "Copy constructor: buffer points to: 0x" << hex << (unsigned int*)buffer << endl;
+        }
+    }
+
+    // move constructor
+
+    MyString(MyString&& moveSource) {
+        cout << "Move constructor: moving from MyString" << endl;
+        if (moveSource.buffer != NULL) {
+            buffer = moveSource.buffer;         // take ownership i.e. move.
+            moveSource.buffer = NULL;           // set the move source to NULL.
         }
     }
 
@@ -54,11 +65,13 @@ public:
 };
 
 MyString Copy(MyString& source) {
+    cout << "Copy entered." << endl;
     MyString copyForReturn(source.GetString());     // create copy.
     return copyForReturn;
 }
 
 int main() {
     MyString sayHello("Hello world of C++");
-    MyString sayHelloAgain(Copy(sayHello));
+    //MyString sayHelloAgain(Copy(sayHello));
+    MyString sayHelloAgain(sayHello);
 }

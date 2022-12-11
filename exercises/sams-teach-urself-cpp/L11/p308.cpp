8a77c147218e698e2acd324803dc7548940d4503
Your Name <you@example.com>
2022-12-10 16:25:10 -0800

    p308.cpp: added

---
+++ exercises/sams-teach-urself-cpp/L11/p308.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Fish {
+public:
+    virtual void Swim() {
+        cout << "Fish swims!" << endl;
+    }    
+};
+
+class Tuna: public Fish {
+public:
+    void Swim() {
+        cout << "Tuna swims." << endl;
+    }
+};
+
+class Carp: public Fish {
+public:
+    void Swim() {
+        cout << "Carp swims." << endl;
+    }
+};
+
+void MakeFishSwim(Fish & inputFish) {
+    inputFish.Swim();
+}
+
+int main() {
+    Tuna MyDinner;
+    Carp MyLunch;
+    
+    MakeFishSwim(MyDinner);
+    MakeFishSwim(MyLunch);
+    return 0;
+}

e0230d9e6501c953ef46ce77a462ec99a1c8739a
Your Name <you@example.com>
2022-12-12 12:43:42 -0800

    benchmark.cpp: added set benchmark support

--- exercises/sams-teach-urself-cpp/STL-benchmarks/benchmark.cpp
+++ exercises/sams-teach-urself-cpp/STL-benchmarks/benchmark.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <set>
+
 using namespace std;
 
 main () {
     // insert 50000, 100000, 150000,200000
     int data_size[]={50000, 100000, 150000, 200000, 250000, 300000};
-    int data_type[]={"vector"}
+    int benchmark_vector[] = {};
+    int benchmark_set[] = {};
 
+    cout << "=========================================";
+    cout << "1. Vector insert benchmar.";
     for (int i = 0 ; i < sizeof(data_size) / sizeof(int) ; i++ ) {
         auto start = std::chrono::high_resolution_clock::now();
 
@@ -16,7 +21,28 @@ main () {
         cout << "Inserting data size: " << data_size[i] << endl;
 
         for (int j = 0; j < data_size[i]; j++ ) {
-            data.push_back(i);
+            data.push_back(j);
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+
+        auto duration = (end - start);
+        auto us = std::chrono::duration_cast<std::chrono::microseconds>(duration); // Microsecond (as int)
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration); // Milliseconds (as int)
+        const float ms_fractional = static_cast<float>(us.count()) / 1000;         // Milliseconds (as float)
+        std::cout << "Duration = " << us.count() << "µs (" << ms_fractional << "ms)" << std::endl;
+    }
+
+    cout << "=========================================";
+    cout << "1. Set insert benchmar.";
+    for (int i = 0 ; i < sizeof(data_size) / sizeof(int) ; i++ ) {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        set<int> data2;
+        cout << "-----------------------------------------" << endl;
+        cout << "Inserting data size: " << data_size[i] << endl;
+
+        for (int j = 0; j < data_size[i]; j++ ) {
+            data2.insert(j);
         }
         auto end = std::chrono::high_resolution_clock::now();
 

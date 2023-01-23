36f6ea977946df638057bb50dd6d971974260bdf
Your Name <you@example.com>
2023-01-23 00:45:51 -0800

    lazy_init-1.cpp: added

--- exercises/misc/build.sh
+++ exercises/misc/build.sh
@@ -1,1 +1,4 @@
-clear; g++ -std=c++17 lazy_init-2.cpp 2>&1 | tee build.log && ./a.out ; nano -w build.log
+for i in 1 2 3; do
+    clear; g++ -std=c++17 lazy_init-$i.cpp 2>&1 | tee build_lazy_init-$i.log && 
+    ./a.out 2>&1 | tee run_lazy_init-$i.log ; 
+done

818d62f56c9a1e08e7cf4d1457d4be243012ea79
Your Name <you@example.com>
2023-02-19 10:04:59 -0800

    std_function_class.cpp: added

---
+++ exercises/misc/std_function/std_function_foo_only.cpp
@@ -0,0 +1,30 @@
+#include <functional>
+#include <iostream>
+ 
+struct Foo {
+    Foo(int num) : num_(num) {}
+    void print_add(int i) const { std::cout << num_+i << '\n'; }
+    int num_;
+};
+ 
+int main()
+{
+    // store a call to a member function
+    std::function<void(const Foo&, int)> f_add_display = &Foo::print_add;
+    const Foo foo(314159);
+    f_add_display(foo, 1);
+    f_add_display(314159, 1);
+ 
+    // store a call to a data member accessor
+    std::function<int(Foo const&)> f_num = &Foo::num_;
+    std::cout << "num_: " << f_num(foo) << '\n';
+ 
+    // store a call to a member function and object
+    using std::placeholders::_1;
+    std::function<void(int)> f_add_display2 = std::bind( &Foo::print_add, foo, _1 );
+    f_add_display2(2);
+ 
+    // store a call to a member function and object ptr
+    std::function<void(int)> f_add_display3 = std::bind( &Foo::print_add, &foo, _1 );
+    f_add_display3(3);
+}

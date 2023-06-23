b02b55dde7e11bd887c09895f73bf5353105d1ac
Your Name <you@example.com>
2023-06-23 18:10:42 +0000

    ex-1-full-explicit.cpp

---
+++ exercises/misc/template/template-specialization/ex-1-full-explicit.cpp
@@ -0,0 +1,25 @@
+/*
+Modification from following example
+https://en.cppreference.com/w/cpp/language/template_specialization
+*/
+
+#include <type_traits>
+#include <iostream>
+
+template<typename T> // primary template
+struct is_void {
+    T value;
+    is_void(T pValue = 10) { value = pValue; }
+};
+
+template<>           // explicit specialization for T = void
+struct is_void<void> {
+    int value = 100;
+};
+ 
+int main()
+{
+    struct is_void<int> s1;
+    struct is_void<void> s2;
+    std::cout << "s1: " << s1.value << ", s2: " << s2.value << std::endl;
+}

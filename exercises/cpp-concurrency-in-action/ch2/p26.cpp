8e19b78711db8a00ad6004c0ffcc1d968d38659e
Your Name <you@example.com>
2024-10-18 22:51:06 -0700

    working state after introducing sleep in each function

--- exercises/cpp-concurrency-in-action/ch2/p26.cpp
+++ exercises/cpp-concurrency-in-action/ch2/p26.cpp
@@ -5,24 +5,27 @@ std::move example.
 #include <thread>
 #include <chrono>
 
-#include <chrono>
-#include <iostream>
-#include <thread>
-
 using namespace std;
-void some_function() { cout << "some_function entered...\n" ; }
-void some_other_function() { cout << "some_other_function entered ...\n"; }
+void some_function() { 
+    cout << "some_function entered...\n" ; 
+    this_thread::sleep_for(chrono::seconds(3));
+}
+void some_other_function() { 
+    cout << "some_other_function entered ...\n"; 
+    this_thread::sleep_for(chrono::seconds(3));
+}
 
 int main() {
     std::thread t1(some_function);
     // t1: some_function
     std::thread t2=std::move(t1);
-    t2.join();
     // t2: some_function
     t1=std::thread(some_other_function);
-    // t2: some function, t1: some_other_function
+    // t1: some_other_function (final)
     t1.join();
+
     std::thread t3;
     t3=std::move(t2);
-    // t2: some function, t3: some_other_function
+    // t3: some_function (final)
+    t3.join();
 }

b326c4801af4afcbbbb852dea8b83e036a93a3b6
Your Name <you@example.com>
2024-10-22 22:15:46 -0700

    p134-acquire-release-2-working.cpp: added

---
+++ exercises/cpp-concurrency-in-action/ch6/p134-acquire-release-2-working.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <atomic>
+#include <assert.h>
+#include <thread>
+
+std::atomic<bool> x,y;
+std::atomic<int> z;
+
+using namespace std;
+
+void write_x_then_y() 
+{
+    y.store(true,std::memory_order_relaxed);
+    x.store(true,std::memory_order_release);
+}
+
+void read_y_then_x() {
+    while(!y.load(std::memory_order_acquire))
+    if(y.load(std::memory_order_relaxed))
+        ++z;
+}
+
+int main() 
+{
+    x=false;
+    y=false;
+    z=0;
+    std::thread a(write_x_then_y);
+    std::thread b(read_y_then_x);
+    a.join();
+    b.join();
+    assert(z.load()!=0);
+}

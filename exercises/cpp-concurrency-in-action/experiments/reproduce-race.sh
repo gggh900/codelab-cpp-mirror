7bf35f3e7483c227c62123c4c3988e8d870ad2e8
Your Name <you@example.com>
2024-10-23 09:54:15 -0700

    reproduce-race.sh: adjusted for new file name in cpp, test line added

--- exercises/cpp-concurrency-in-action/experiments/reproduce-race.sh
+++ exercises/cpp-concurrency-in-action/experiments/reproduce-race.sh
@@ -5,4 +5,6 @@ sudo g++ reproduce-race.cpp && echo "" | sudo tee reproduce-race.txt
 echo "starting /a.out, reproducer...will take a while with 1000000 loop" 
 sudo ./a.out
 echo "done"
-egrep -v "1 \+ 2 = 3" reproduce-race.txt | head -5
+egrep -v "1 \+ 2 = 3" reproduce-race-* | head -5
+#for test only:
+egrep  "1 \+ 2 = 3" reproduce-race-* | head -5

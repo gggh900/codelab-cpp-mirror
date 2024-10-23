1f9ca360a783340484daf14e37b6c30523059db6
Your Name <you@example.com>
2024-10-23 09:43:02 -0700

    reproduce-race.sh: grep cmd added

--- exercises/cpp-concurrency-in-action/experiments/reproduce-race.sh
+++ exercises/cpp-concurrency-in-action/experiments/reproduce-race.sh
@@ -1,1 +1,5 @@
-sudo g++ reproduce-race.cpp && echo "" | sudo tee reproduce-race.txt ; nohup sudo ./a.out &
+sudo g++ reproduce-race.cpp && echo "" | sudo tee reproduce-race.txt ; 
+echo "starting /a.out, reproducer...will take a while with 1000000 loop" 
+sudo ./a.out
+echo "done"
+egrep -v "1 \+ 2 = 3" reproduce-race.txt | head -5

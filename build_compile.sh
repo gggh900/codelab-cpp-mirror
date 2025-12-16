b00bb4b5075da0e177fbf0dc3216faaedc55b6be
Your Name <you@example.com>
2025-12-16 16:26:38 -0600

    build_compile.sh: added

---
+++ build_compile.sh
@@ -0,0 +1,11 @@
+set -x
+pushd exercises/sams-teach-urself-cpp/L9 
+mkdir out
+ret=0
+for i in ./*.cpp ; do
+    echo '--------- building and running $i ... ---------'
+    g++ $i -o out/$i.out && ./out/$i.out
+    [[ $? -ne 0 ]] && ret=1
+done
+popd
+exit $ret
